86cd190fbda08aebbb1d05b69cfffad46b300a8f
ahmtgckl <ahmtgckl@gmail.com>
2021-10-29 12:11:38 +0300

    jscamo

---
+++ src/app.js
@@ -0,0 +1,19 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarBugun = 9.30
+let dolarDun = 9.20
+console.log(dolarDun)
+
+const euroDun = 11.2
+console.log(euroDun)
+
+//array
+//camelCasing  fonksiyon tanımlama
+//PascalCasing  method isimleri
+let konutKredileri = ["Konut kredisi","Emlak konut kredisi","Kamu konut kredisi","Özel konut kredileri"]
+
+console.log("<ul>")
+for (let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
